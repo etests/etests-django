ecd37d3c05a6a00702eba2deb7536655ffb99288
Piyush Maurya <piyushkumarmaurya@gmail.com>
2019-05-01 20:08:34 +0530

    Initialized project directories
    - backend: Django with rest_framework
    - frontend: Vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img id="logo" src="./assets/logos/logo_plain.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#logo {
+  width: 200px;
+  height: 200px;
+  image-rendering: align;
+  border-radius: 8px;
+  box-shadow: 2px 2px 20px #2c3e50;
+}
+</style>
