ef8949c1000a488f9c0eb707b5bfed15ba02bb24
Piyush Maurya <piyushkumarmauyra@gmail.com>
2019-06-03 12:12:57 +0530

    Modified serializers and viewsets for user registration

--- backend/eusers/serializers.py
+++ backend/eusers/serializers.py
@@ -2,13 +2,31 @@ from .models import *
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("name", "email", "phone", "state", "city")
+        extra_kwargs = {'password': {'write_only': True}}
+        fields = ("id", "name", "email", "phone", "state", "city", "password", "is_student", "is_institute")
 
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
 
-class StudentSerializer(serializers.HyperlinkedModelSerializer):
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.state = validated_data.get('state', instance.state)
+        instance.city = validated_data.get('city', instance.city)
+        if validated_data.get('password', None):
+            instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
+
+class StudentSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=True)
 
     class Meta:
@@ -27,17 +45,18 @@ class StudentSerializer(serializers.HyperlinkedModelSerializer):
         return student
 
 
-class InstituteSerializer(serializers.HyperlinkedModelSerializer):
+class InstituteSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=True)
 
     class Meta:
         model = Institute
-        fields = ("user", "pincode")
+        extra_kwargs = {'pk': {'read_only': True}}
+        fields = ("pk", "user", "pincode")
 
     def create(self, validated_data):
         user_data = validated_data.pop("user")
         user = UserSerializer.create(UserSerializer(), validated_data=user_data)
         institute, created = Institute.objects.update_or_create(
-            user=user, gender=validated_data.pop("pincode")
+            user=user, pincode=validated_data.pop("pincode")
         )
         return institute
--- backend/eusers/views.py
+++ backend/eusers/views.py
@@ -1,17 +1,30 @@
 from .models import *
 from .serializers import *
 from rest_framework import viewsets
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import BasePermission, IsAuthenticatedOrReadOnly, AllowAny, IsAdminUser, SAFE_METHODS
 
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.owner == request.user
 
 class UserViewSet(viewsets.ModelViewSet):
+    authentication_classes=[JSONWebTokenAuthentication, SessionAuthentication]
+    permission_classes=[IsAdminUser, IsOwnerOrReadOnly]
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
 class StudentViewSet(viewsets.ModelViewSet):
+    permission_classes=[AllowAny]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 class InstituteViewSet(viewsets.ModelViewSet):
+    permission_classes=[AllowAny]
     queryset = Institute.objects.all()
     serializer_class = InstituteSerializer
 
