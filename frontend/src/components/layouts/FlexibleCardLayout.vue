1bba3c1d994200aa5f679b9936605e94a3f2b1f4
pankaj7822 <pankaj.krjha.cse17@itbhu.ac.in>
2019-09-27 02:37:33 +0530

    moved frontend to vue-etests

--- frontend/src/components/layouts/FlexibleCardLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-layout justify-center align-center>
-    <v-flex xs12 md8 lg6>
-      <v-card min-height="300px">
-        <slot></slot>
-      </v-card>
-    </v-flex>
-  </v-layout>
-</template>
