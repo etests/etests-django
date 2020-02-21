23781ac6605c8b9124c964de88e07bddbe08c180
Piyush Maurya <piyushkumarmaurya@gmail.com>
2020-02-21 22:24:29 +0530

    Email tracking

--- api/ses.py
+++ api/ses.py
@@ -2,39 +2,25 @@ import boto3
 from botocore.exceptions import ClientError
 from env import EMAIL_ID, AWS_REGION
 
+
 def send_email(
-to_email,
-subject,
-body_html,        
-from_email = "noreply@etests.co.in",
-charset = "UTF-8"
+    to_email, subject, body_html, from_email="noreply@etests.co.in", charset="UTF-8"
 ):
-    client = boto3.client('ses',region_name=AWS_REGION)
+    client = boto3.client("ses", region_name=AWS_REGION)
     try:
         response = client.send_email(
-            Destination={
-                'ToAddresses': [
-                    to_email,
-                ],
-            },
+            Destination={"ToAddresses": [to_email]},
             Message={
-                'Body': {
-                    'Html': {
-                        'Charset': charset,
-                        'Data': body_html,
-                    },
-                },
-                'Subject': {
-                    'Charset': charset,
-                    'Data': subject,
-                },
+                "Body": {"Html": {"Charset": charset, "Data": body_html}},
+                "Subject": {"Charset": charset, "Data": subject},
             },
             Source=from_email,
+            ConfigurationSetName="EmailDelivery",
         )
     except ClientError as e:
-        print(e.response['Error']['Message'])
+        print(e.response["Error"]["Message"])
         return False
     else:
         print("Email sent! Message ID:"),
-        print(response['MessageId'])
+        print(response["MessageId"])
         return True
