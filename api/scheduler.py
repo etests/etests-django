0ac11fff4b28fc97bfa424d2058a7c280a9c43f5
Piyush Maurya <piyushkumarmaurya@gmail.com>
2020-06-27 21:49:44 +0530

    Update cron times

--- api/scheduler.py
+++ api/scheduler.py
@@ -11,5 +11,5 @@ def start():
     # email_institutes_test_report, "cron", day_of_week="thu,mon", hour="2"
     # )
     scheduler.add_job(evaluate_sessions, "cron", day_of_week="*", hour="2")
-    scheduler.add_job(auto_rank_declare, "cron", day_of_week="*", hour="2")
+    scheduler.add_job(auto_rank_declare, "cron", day_of_week="*", hour="3")
     scheduler.start()
